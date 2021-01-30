2d7f3cf0b4481aa036f973ad36f54c655a6ca84c
BooStarGamer <marti.buxeda@gmail.com>
2021-01-30 12:53:06 +0100

    Semisolid draw & rects finished

--- Game/Source/Semiground_Tile.cpp
+++ Game/Source/Semiground_Tile.cpp
@@ -47,6 +47,11 @@ void SemigroundTile::Draw()
 	bool downRot = false;
 	bool leftRot = false;
 	bool rightRot = false;
+	bool upBoth = false;
+	bool downBoth = false;
+	bool leftBoth = false;
+	bool rightBoth = false;
+	bool bothT = false;
 
 	ListItem<SemigroundTile*>* list;
 	for (list = app->scene->sceneEditor->semigroundTiles.start; list != nullptr; list = list->next)
@@ -55,72 +60,112 @@ void SemigroundTile::Draw()
 		{
 			up = true;
 			if (list->data->rotated) upRot = true;
+			if (list->data->both) upBoth = true;
 		}
 		if (list->data->coordinates == downTile)
 		{
 			down = true;
 			if (list->data->rotated) downRot = true;
+			if (list->data->both) downBoth = true;
 		}
 		if (list->data->coordinates == leftTile)
 		{
 			left = true;
 			if (list->data->rotated) leftRot = true;
+			if (list->data->both) leftBoth = true;
 		}
 		if (list->data->coordinates == rightTile)
 		{
 			right = true;
 			if (list->data->rotated) rightRot = true;
+			if (list->data->both) rightBoth = true;
 		}
 
 		if (up && left && down && right) break;
 	}
 
-	if (!rotated)
+	if (left && !leftRot && !leftBoth && down && downRot)
 	{
-		if (!left && !right)
+		bothT = true;
+		rotated = false;
+		semigroundRect = semigroundHRect;
+	}
+
+	both = bothT;
+
+	if (!both)
+	{
+		if (!rotated)
 		{
-			app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
+			if (!left && !right)
+			{
+				app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
+			}
+			else if (left && right)
+			{
+				if (!leftRot && !rightRot)
+				{
+					if (!leftBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground2);
+					else if (leftBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5);
+				}
+				else if (!leftRot && rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4);
+				else if (leftRot && !rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5);
+				else if (leftRot && rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
+			}
+			else if (!left && right)
+			{
+				if (!rightRot)
+				{
+					app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5);
+				}
+				else if (rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
+			}
+			else if (left && !right)
+			{
+				if (!leftRot)
+				{
+					if (!leftBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4);
+					else if (leftBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
+				}
+				else if (leftRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
+			}
 		}
-		else if (left && right)
+		else if (rotated)
 		{
-			if (!leftRot && !rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground2);
-			else if (!leftRot && rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4);
-			else if (leftRot && !rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5);
-			else if (leftRot && rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
-		}
-		else if (!left && right)
-		{
-			if (!rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5);
-			else if (rightRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
-		}
-		else if (left && !right)
-		{
-			if (!leftRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4);
-			else if (leftRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1);
+			if (!up && !down)
+			{
+				app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1, 1.0f, 90);
+			}
+			else if (up && down)
+			{
+				if (upRot && downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground2, 1.0f, 90);
+				else if (upRot && !downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4, 1.0f, 90);
+				else if (!upRot && downRot)
+				{
+					if (!upBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5, 1.0f, 90);
+					else if (upBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground2, 1.0f, 90);
+				}
+				else if (!upRot && !downRot)
+				{
+					if (!upBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1, 1.0f, 90);
+					else if (upBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4, 1.0f, 90);
+				}
+			}
+			else if (!up && down)
+			{
+				if (downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5, 1.0f, 90);
+				else if (!downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1, 1.0f, 90);
+			}
+			else if (up && !down)
+			{
+				if (upRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4, 1.0f, 90);
+				else if (!upRot)
+				{
+					if (!upBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1, 1.0f, 90);
+					else if (upBoth) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4, 1.0f, 90);
+				}
+			}
 		}
 	}
-	else if (rotated)
-	{
-		if (!up && !down)
-		{
-			app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1, 1.0f, 90);
-		}
-		else if (up && down)
-		{
-			if (upRot && downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground2, 1.0f, 90);
-			else if (upRot && !downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4, 1.0f, 90);
-			else if (!upRot && downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5, 1.0f, 90);
-			else if (!upRot && !downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1, 1.0f, 90);
-		}
-		else if (!up && down)
-		{
-			if (downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground5, 1.0f, 90);
-			else if (!downRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1, 1.0f, 90);
-		}
-		else if (up && !down)
-		{
-			if (upRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground4, 1.0f, 90);
-			else if (!upRot) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground1, 1.0f, 90);
-		}
-	}
+	else if (both) app->render->DrawTexture(semigroundTex, position.x, position.y, &semiground3);
 }
